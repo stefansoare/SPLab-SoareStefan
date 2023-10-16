35cee669029a79f7ad4bcbbbb76edb4e4c0890f5
Soare Stefan <stefan.soare02@e-uvt.ro>
2023-10-16 18:25:49 +0300

    new project

---
+++ src/main/java/com/example/designpatternslab2023/DesignPatternsLab2023Application.java
@@ -0,0 +1,13 @@
+package com.example.designpatternslab2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsLab2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsLab2023Application.class, args);
+    }
+
+}
